f150ff10a8a268bcbf4a51221cecbbe7fb4d5fa1
owen <owencita32@gmail.com>
2025-07-07 22:35:07 +0700

    base

--- vite.config.js
+++ vite.config.js
@@ -3,11 +3,12 @@ import laravel from "laravel-vite-plugin";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-    plugins: [
-        laravel({
-            input: ["resources/css/app.css", "resources/js/app.js"],
-            refresh: true,
-        }),
-        tailwindcss(),
-    ],
+	base: "https://veblog-production.up.railway.app/",
+	plugins: [
+		laravel({
+			input: ["resources/css/app.css", "resources/js/app.js"],
+			refresh: true,
+		}),
+		tailwindcss(),
+	],
 });
